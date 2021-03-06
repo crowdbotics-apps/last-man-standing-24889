38d4be7fb7249cd00f87de1ed5bfc2254f087c7b
Crowdbotics <team@crowdbotics.com>
2021-03-06 23:45:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/last_man_standing_24889/MainActivity.java
@@ -0,0 +1,15 @@
+package com.last_man_standing_24889;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "last_man_standing_24889";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://last-man-standing-24889.botics.co",
+  defaultTimeout: 5000
+};
